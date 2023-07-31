0933972fcdc5388e0d6eaf8f9b0f17092494bb01
João V. Frassi <joaovictorfrassi@gmail.com>
2023-07-31 15:52:59 -0300

    aula sobre variáveis em javascript

---
+++ Estrutura de programação/1_variaveis/script.js
@@ -0,0 +1,9 @@
+let nome = "joao";
+
+console.log(nome)
+console.log(`O meu nome é ${nome}`)
+
+nome = "jeferson"
+
+console.log(nome)
+
---
+++ Estrutura de programação/2_variaveisConstVar/script.js
@@ -0,0 +1,12 @@
+var nome = "matheus";
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+nome = "joao"
+ip = "123"
+
+// nao pode começar variável com número, mas pode terminar com um
+// pode ter $ e _,mas nao podem ter outros caracteres especiais
+// pode iniciar com letra maiúscula, mas geralmente se utiliza Camelcase
