97e64cb91d78b69e0ec77ec7ee528eb1230bab1d
João V. Frassi <joaovictorfrassi@gmail.com>
2023-07-31 17:09:46 -0300

    estruturas de repetição pt.1

---
+++ Estrutura de programação/4_if/script.js
@@ -0,0 +1,16 @@
+let idade = 17;
+if(idade == 19){
+    console.log('A idade é 19');
+}
+if(idade<19){
+    console.log('A idade é menor que 19');
+}
+if(idade>25){
+    console.log("A idade é maior que 25");
+}
+
+let nome = "joao";
+
+if(idade>10 && nome == "joao"){
+    console.log("Bem vindo joao");
+}
