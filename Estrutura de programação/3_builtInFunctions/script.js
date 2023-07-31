b6d05f70011741251387a25331c0a9020d38c989
João V. Frassi <joaovictorfrassi@gmail.com>
2023-07-31 16:47:45 -0300

    funções built-in

---
+++ Estrutura de programação/3_builtInFunctions/script.js
@@ -0,0 +1,13 @@
+let idade = prompt('Qual a sua idade?');
+console.log(idade);
+
+let nome = prompt("qual o seu nome?");
+
+console.log(`O seu nome é ${nome}`)
+
+
+alert('Veja esta mensagem')
+
+
+let = maiorNumero = Math.min(1,3,10,15,78,98)
+console.log(maiorNumero)
