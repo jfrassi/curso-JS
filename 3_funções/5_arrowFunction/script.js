020fa3aefa04a6a8e6e857632fb2ffc91dd37096
João V. Frassi <joaovictorfrassi@gmail.com>
2023-08-15 15:50:54 -0300

    arrow function

---
+++ 3_funções/5_arrowFunction/script.js
@@ -0,0 +1,24 @@
+
+let consoleTeste = () =>{
+    console.log("Olá mundo!")
+}
+
+consoleTeste();
+
+let soma = (a,b) =>{
+    return a + b;
+}
+
+console.log(soma(10,20));
+
+
+/// mais sobre arrow function
+
+const raizQuadrada = (x) => {
+    return x * x;
+}
+
+const raizQuadrada2 = n => n * n;
+
+console.log(raizQuadrada(13))
+console.log(raizQuadrada2(13))
