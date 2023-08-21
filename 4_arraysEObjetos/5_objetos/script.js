19a38c6ab9f487f3fd3d37a5833db15e174a4cea
João V. Frassi <joaofrassi.dev@gmail.com>
2023-08-21 16:25:32 -0300

    aprendendo a adicionar e deletar propriedades em um objeto

--- 4_arraysEObjetos/5_objetos/script.js
+++ 4_arraysEObjetos/5_objetos/script.js
@@ -1,18 +1,7 @@
-let pessoa = {
-    nome: "joao",
-    profissao: "estudante",
-    idade: 22,
-};
-
-console.log(pessoa.nome);
-console.log(pessoa.idade);
-console.log(pessoa.profissao);
-
-
 let cachorro = {
     patas: 4 ,
     nome: 'Shark',
-    latir: function() {
+    latir: function () {
         console.log("Au Au");
     }
 }
---
+++ 4_arraysEObjetos/6_objetosAddEDel/script.js
@@ -0,0 +1,21 @@
+let pessoa = {
+    nome: "joao",
+    idade: 22,
+    profissao: "estudante",
+}
+
+console.log(pessoa.nome);
+
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+
+console.log(pessoa);
+
+pessoa.casado = false;
+
+
+console.log(pessoa.casado)
+
+////
+
