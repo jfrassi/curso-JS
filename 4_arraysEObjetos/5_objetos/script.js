78fe6fd32368edfcb84882ebfa144dd376155d08
João V. Frassi <joaofrassi.dev@gmail.com>
2023-08-21 16:16:45 -0300

    aula sobre objetos

---
+++ 4_arraysEObjetos/5_objetos/script.js
@@ -0,0 +1,19 @@
+let pessoa = {
+    nome: "joao",
+    profissao: "estudante",
+    idade: 22,
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.profissao);
+
+
+let cachorro = {
+    patas: 4 ,
+    nome: 'Shark',
+    latir: function() {
+        console.log("Au Au");
+    }
+}
+cachorro.latir();
