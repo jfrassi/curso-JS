d8827030adc3f811236df5e558e0efbb545764c1
João V. Frassi <joaofrassi.dev@gmail.com>
2023-08-22 16:33:00 -0300

    metodos de arrays

---
+++ 4_arraysEObjetos/10_metodosDeArrays/script.js
@@ -0,0 +1,31 @@
+let pessoas = [ "matheus", 'joao', 'ricardo'];
+
+/*let pessoaRemovida = pessoas.pop();
+
+
+console.log(pessoaRemovida)
+
+let pessoaRemovida2 = pessoas.pop();
+
+console.log(pessoaRemovida2)*/
+
+pessoas.push("maria");
+
+console.log(pessoas);
+
+///shift e unshift
+
+let frutas = ['maçã', 'melão', 'uva'];
+
+let frutaRemovida = frutas.shift();
+
+
+console.log(frutaRemovida);
+
+frutas.unshift("morango");
+
+console.log(frutas[0])
+
+/// index of e last index of
+
+let numeros = [0, 1, 2, 1, 0]
