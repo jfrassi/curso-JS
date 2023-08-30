3fcfa111be4b850917bfe4d67a1255f7f684a79f
João V. Frassi <joaofrassi.dev@gmail.com>
2023-08-30 16:30:35 -0300

    mais sobre prototypes

---
+++ 5_POO/5_maisSobrePrototypes/script.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    maos: 2
+}
+
+/*console.log(Object.getPrototypeOf(pessoa));
+console.log(Object.getPrototypeOf(pessoa) == Object.prototype);
+*/
+
+const pessoaNova = Object.create(pessoa);
+console.log(pessoaNova.maos);
+console.log(pessoaNova.hasOwnProperty('maos'));
+console.log(Object.getPrototypeOf(pessoaNova)==pessoa)
