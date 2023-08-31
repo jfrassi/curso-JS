acf934b353b73b7f73e198607f5a1b059934cb2d
João V. Frassi <joaofrassi.dev@gmail.com>
2023-08-31 16:44:10 -0300

    override nos prototypes clear . . .

---
+++ 5_POO/12_overridePrototype/script.js
@@ -0,0 +1,12 @@
+class Cachorro {
+    constructor(raca){
+        this.raca = raca;
+    }
+}
+
+let poodle = new Cachorro('Poodle');
+
+Cachorro.prototype.raca = "SRD"
+
+console.log(poodle.raca);
+console.log(Cachorro.prototype.raca);
