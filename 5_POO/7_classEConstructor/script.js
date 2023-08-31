6278fe78d8208d181ffabb3273cc21510af32f45
João V. Frassi <joaofrassi.dev@gmail.com>
2023-08-31 15:44:33 -0300

    criar classe por construtor

---
+++ 5_POO/7_classEConstructor/script.js
@@ -0,0 +1,16 @@
+
+///instanciar = criar um novo objeto
+
+/// no construtor ja podemos definir propriedades
+
+function criarCachorro(raca, patas, cor) {
+    let cachorro = Object.create({})
+    cachorro.raca = raca;
+    cachorro.patas = patas;
+    cachorro.cor = cor;
+    return cachorro;
+}
+
+let doberman = criarCachorro('Doberman', 4, 'preta');
+
+console.log(doberman); 
